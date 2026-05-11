c05a1a953831e7dec6507d4e2bbed3483bd29c83
amit45-creator <akumargkp2021@gmail.com>
2026-05-11 14:46:24 +0530

    datatypes

---
+++ dataTypes/datatypes.js
@@ -0,0 +1,21 @@
+"use strict"  ;// treat all Js code as newer version 
+// alert (3+3)// js ka engine browser ke andar chipa hua hai // we are not using nodejs, not  browser 
+
+// code readability  should be high
+let name ="hitesh"
+let age =18
+let isLoggedIn=false
+let state ;
+
+//bigint
+//null =>standalone value 
+// undefined =>
+//symbol => unique
+
+
+
+// object 
+
+console.log(typeof null);  //object
+console.log(typeof undefined);//undefined
+
