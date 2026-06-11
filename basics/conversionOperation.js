bcb4e90a8e8bec869551bf3b0aa80fe40c7f82a3
amit45-creator <akumargkp2021@gmail.com>
2026-06-12 00:35:34 +0530

    Updated conversion operations examples

--- basics/conversionOperation.js
+++ basics/conversionOperation.js
@@ -3,8 +3,9 @@ let score ="33"
 console.log(typeof score);
 console.log(typeof(score));
 
-let valueInNumber = Number(score)
+let valueInNumber = Number(score) // string to  number conversion 
 console.log(typeof  valueInNumber);
+
 
 let isLoggedIn=1
  let booleanIsLoggedIn= Boolean(isLoggedIn)
@@ -12,9 +13,30 @@ let isLoggedIn=1
 
 
  let someNumber =33  
-
  let stringNumber =String(someNumber)
  console.log(string);
+ console.log(typeof stringNumber);
+
+ 
 
 
+// operatoins 
 
+console.log("1" +2+2);//122
+console.log(1+2+"2");//32
+// preincrement and postincrement documentations 
+
+
+console.log("2">1);//true
+console.log("02" >1);//true
+
+console.log(null>0);//false
+console.log(null==0);//false
+console.log(null>=0);//true
+
+console.log(undefined==0);
+console.log(undefined>0);
+console.log(undefined<0);//   false
+
+
+//===  also  check data types 
