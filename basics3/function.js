3b6baad4305834618d2daec70de6d9f0363c8b78
amit45-creator <akumargkp2021@gmail.com>
2026-07-22 18:13:01 +0530

    basics3

--- basics3/function.js
+++ basics3/function.js
@@ -33,3 +33,37 @@ function loginUserMessage(username="sam"){
 //  console.log(loginUserMessage(""))
 //  console.log(loginUserMessage())//undefined
 
+function calculateCartPrice(...num1){//saara khel dot ka hai 
+    return num1
+}
+// console.log(calculateCartPrice(200,400,500))
+
+
+function calculateCartPrice(val,val2,...num1){//saara khel dot ka hai 
+    return num1
+}
+// console.log(calculateCartPrice(200,400,500,2000))
+
+const user ={
+    username: "hitesh",
+    price: 199
+}//it is object
+
+function handleObject (anyobject){
+    console.log(`Username is${anyobject.username} and price is ${anyobject.price}`);
+}
+// handleObject(user)
+handleObject({
+    username: "sam",
+    price: 399
+})//we can also pass direct object 
+
+
+const myNewArray =[200,400,100,600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+
+}
+//  console.log(returnSecondValue(myNewArray));
+ console.log(returnSecondValue([200,400,500,1000]));
