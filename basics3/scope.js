88c468213c3bbd324d083dd6ed03aac2874a5bb6
amit45-creator <akumargkp2021@gmail.com>
2026-07-26 02:29:13 +0530

    function

--- basics3/scope.js
+++ basics3/scope.js
@@ -15,3 +15,49 @@ console.log(a);
 // console.log(c);
 
 //globe scope and block scope 
+
+
+
+//another example
+// function one() {
+//     const username ="hitesh" 
+//     function two() {
+//         const website ="youtube"
+//         console.log(username);     //closure concept ?
+//     }
+//     console.log(website);// yaha error aayega kyuki iska scope keval block tak hi hai 
+
+//     two();
+// }
+// one();
+
+
+// //another example 
+// if(true) {
+//     const username ="amit "
+//     if(username==="hitesh"){
+//         const website = "youtube"
+//         console.log(username+website);
+//     }
+//     console.log(website);
+// }
+// console.log(username);
+
+
+
+addone(5)//no error
+function addone (value){
+    return num +1
+}
+
+
+
+// addTwo(4)//error aayega yaha 
+// const addTwo=function(num){
+//     return num+2
+// }
+
+
+
+
+
