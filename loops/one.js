b68fdc76b6ff50e049373adbc07d93d562a73e18
amit45-creator <akumargkp2021@gmail.com>
2026-08-03 21:09:54 +0530

    loop

---
+++ loops/one.js
@@ -0,0 +1,18 @@
+// for 
+
+for(let index=0;index<10;index++){
+    const element =index;
+    console.log(element);
+}
+
+// break and continue 
+
+
+for(let index=1;index<=20;index++){
+    if(index==5){
+        console.log(`Detected 5`);
+        break
+    }
+
+    console.log(`value of i is ${index}`);
+}
